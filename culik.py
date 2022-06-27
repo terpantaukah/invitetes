fbabd7d17bbe80c7f2195b9792ea280dd12c87e2
terpantaukah <99101419+terpantaukah@users.noreply.github.com>
2022-06-27 22:03:47 +0700

    Create culik.py

---
+++ culik.py
@@ -0,0 +1,87 @@
+import os
+from pyrogram import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.environ.get("API_ID", "6"))
+API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+STRING_SESSION1 = os.environ.get("STRING_SESSION1")
+STRING_SESSION2 = os.environ.get("STRING_SESSION2")
+STRING_SESSION3 = os.environ.get("STRING_SESSION3")
+STRING_SESSION4 = os.environ.get("STRING_SESSION4")
+STRING_SESSION5 = os.environ.get("STRING_SESSION5")
+
+
+bot1 = (
+    Client(
+        session_name=STRING_SESSION1,
+        api_id=API_ID,
+        api_hash=API_HASH
+    )
+    if STRING_SESSION1
+    else None
+)
+
+bot2 = (
+    Client(
+        session_name=STRING_SESSION2,
+        api_id=API_ID,
+        api_hash=API_HASH
+    )
+    if STRING_SESSION2
+    else None
+)
+
+bot3 = (
+    Client(
+        session_name=STRING_SESSION3,
+        api_id=API_ID,
+        api_hash=API_HASH
+    )
+    if STRING_SESSION3
+    else None
+)
+
+bot4 = (
+    Client(
+        session_name=STRING_SESSION4,
+        api_id=API_ID,
+        api_hash=API_HASH
+    )
+    if STRING_SESSION4
+    else None
+)
+
+bot5 = (
+    Client(
+        session_name=STRING_SESSION5,
+        api_id=API_ID,
+        api_hash=API_HASH
+    )
+    if STRING_SESSION5
+    else None
+)
+
+bots = [bot for bot in [bot1, bot2, bot3, bot4, bot5] if bot]
+
+
+print("Selamat datang di pyrogram adder members")
+
+bahan = input("silahkan masukan username group yang mau di culik")
+target = input("silahkan masukan username yang mau di tambahkan")
+
+
+for bot in bots:
+    client = await get_client(bot)
+    try:
+        async for member in client.iter_chat_members(bahan):
+            user = member.user
+            zxb = ["online", "offline", "recently", "within_week"]
+            if user.status in zxb:
+                try:
+                    await client.add_chat_members(target, user.id, forward_limit=100)
+                except Exception as e:
+                    print(5)
+    except:
+        pass
